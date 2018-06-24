16e183b4f09c89777c11d6a12ae2a1152d61bc63
yinyee <anon@anons-MacBook-Air.local>
2018-06-24 12:31:07 +0100

    Include script for local host

--- script_local.sh
+++ script_local.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 
 # Create a new user
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 1.23,"longitude": 4.56,"timestamp":123456789},"appearance": {"gender": "male","hair": "pink","shirt": "red","shorts": "green","socks": "blue","shoes": "blue"}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5239569,"longitude": -0.0882439,"timestamp":123456789},"appearance": {"gender": "male","hair": "pink","shirt": "red","shorts": "green","socks": "blue","shoes": "blue"}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
+echo
 
 # New user issues a challenge
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
+echo
+echo
 
 # New user updates location several times
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 1.77,"longitude": 4.77}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5242974,"longitude": -0.0863556}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
 
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
 
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 2.77,"longitude": 4.77}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
+echo
+sleep 2
+
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5250216,"longitude": -0.085111}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
 
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
 
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 3.77,"longitude": 4.77}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
+echo
+sleep 2
+
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5248439,"longitude": -0.0852532}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
 
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
 
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 4.77,"longitude": 4.77}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
+echo
+sleep 2
+
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5239494,"longitude": -0.0854115}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
 
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
 
-curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 5.77,"longitude": 4.77}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
+echo
+sleep 2
+
+curl -X PUT --data '{"id": "00112233-4455-6677-8899-aabbccddeefg","location": {"latitude": 51.5239569,"longitude": -0.0860552}, "appearance": {}}' -H "Content-Type:application/json" http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg
+echo
 
 curl -X GET http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
+
+echo
+echo
+
+# Delete the challenge
+curl -X DELETE http://127.0.0.1:3000/user/00112233-4455-6677-8899-aabbccddeefg/challenger
+echo
